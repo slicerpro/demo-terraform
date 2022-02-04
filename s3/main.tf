3f207986b1251c7f814c6f44d08a49c20e408d2e
slicerpro <ron.maroria@gmail.com>
2022-02-03 19:34:32 -0600

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -1,1 +1,28 @@
+#setup terraform cloud and workspace
+terraform {
+  backend = "remote" {
+    hostname = "app.terraform.io"
+    organization = "2022-saa"
+    
+    workspaces {
+      name = "demo-terraform"
+    }
+  }
+}
 
+#setup terraform providers
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.72.0"
+    }
+  }
+  
+  required_version = "1.1.3"
+}
+
+#setup AWS provider
+provider "aws"{
+  provider = var.aws_region
+}
