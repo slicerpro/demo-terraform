66449cf65dd271478a9c8183dd10437317fbcbbd
slicerpro <ron.maroria@gmail.com>
2022-02-03 19:50:28 -0600

    Update s3.tf

--- s3/s3.tf
+++ s3/s3.tf
@@ -1,8 +1,8 @@
 #Setup s3
 resource "aws_s3_bucket" "2022saa_demo_terraform_s3" {
-  bucket = "2022-demo-terraform-s3"
+  bucket = "_2022-demo-terraform-s3"
   
   tags = {
-    Name = "2022-dem0-terraform-s3"
+    Name = "_2022-dem0-terraform-s3"
   }
 }
